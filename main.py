115cee550f9a27ee882fa7f2e8a285095e24c0e3
Phyo Thet Pai <phyo.thet.pai@sji.edu.sg>
2026-07-04 09:42:39 +0800

    classifying sentences and more color coding

--- main.py
+++ main.py
@@ -9,19 +9,57 @@ A vacuole is a membrane-bound organelle which is present in plant and fungal cel
 """
 text = re.sub(r"\[\d+]", '', text) # for wikipedia
 doc = nlp(text)
-sentences = doc.sents
-for sentence in sentences:
-    tokens = [i for i in sentence if i.pos_ not in ["SPACE","PUNCT"]]
-    roots = [i for i in sentence if i.dep_ == "ROOT"]
-    for root in roots:
-        tree = Tree(f"{root.text}\t{root.lemma_}")
 
+for sentence in doc.sents:
+    tokens = [i for i in sentence if i.pos_ not in ["SPACE"]]
+    root = sentence.root
+    def create_tree():
+        colors = {
+            "NOUN": "yellow",
+            "PROPN": "yellow",
+            "ADJ": "green",
+            "ADV": "green",
+            "VERB": "blue",
+            "PRON": "dark_orange",
+        }
+        tree = Tree(f"[bold {colors.get(root.pos_,"")}]{root.text}[/bold {colors.get(root.pos_,"")}]\t{root.lemma_}\t{root.pos_}/{root.tag_}")
         def add_branch(upstream, parent):
             for token in tokens:
                 if token.head == parent and token != parent:
-                    branch_text = f"{token.text}\t{token.dep_}\t{token.pos_}"
-                    branch = upstream.add(branch_text)
 
+                    branch_text = f"[bold {colors.get(token.pos_,"")}]{token.text}[/bold {colors.get(token.pos_,"")}]\t{token.dep_}\t{token.pos_}/{token.tag_}"
+                    branch = upstream.add(branch_text)
                     add_branch(branch, token)
         add_branch(tree, root)
         print(tree)
+    create_tree()
+    # print([i for i in sentence.noun_chunks])
+    def classify_sentence():
+        match root.pos_:
+            case "NOUN"|"PROPN":
+                print("Answer")
+            case "VERB":
+                if nsubj := next((i for i in tokens if i.dep_ in ["nsubj","nsubjpass"]), None):
+                    if (punct := next((i for i in reversed(tokens) if i.dep_ in ["punct"]), None)) and punct.text == "?":
+                        print("Question")
+                    else:
+                        print("Declaration")
+                else:
+                    print("Command")
+            case "AUX":
+                if (punct := next((i for i in reversed(tokens) if i.dep_ in ["punct"]), None)) and punct.text == "?":
+                    print("Question")
+                else:
+                    print("Declaration")
+
+    # if root.pos_ == "AUX":
+    #     subj = next((i for i in tokens if i.dep_ == "nsubj"), None)
+    #     if subj:
+    #         subj_topic = Topic(subj.lemma_)
+    #
+    #         adj = next((i for i in tokens if i.dep_ == "acomp"), None)
+    #         if adj:
+    #             adj_topic = Topic(adj.lemma_)
+    #             # adj_topic.mod.append()
+    #             relations.append(Relation(subj_topic, adj_topic))
+    # print([str(i) for i in relations])
